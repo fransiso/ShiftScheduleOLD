e4b4aefcc19f1be8801f03ec58db243b914c48d1
fransiso <myfckmail@hotmail.com>
2014-10-02 17:55:32 +0300

    include solution file & nuget configuration

---
+++ src/02-Domain Services/ShiftSchedule.Domain.Services/EmployeeService.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using ShiftSchedule.Domain.Entities;
+
+namespace ShiftSchedule.Domain.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IEmployeeService _repository;
+
+        public EmployeeService(IEmployeeService repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Employee> GetEmployees()
+        {
+			return _repository.GetEmployees();
+        }
+    }
+}
